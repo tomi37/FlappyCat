faf0a96c042e6e21e2a4f8dac603526bf5a3edbf
tomi37 <skhope_n0233@nifty.com>
2021-02-24 20:33:53 +0900

    first commit

---
+++ FlappyCat/Assets/Script/CatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    private Rigidbody2D rigid2D;
+    private float jumpForce = 680.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプする
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+    }
+}
---
+++ FlappyCat/Assets/Script/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public float moveLeft = -3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveLeft * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < -9.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
